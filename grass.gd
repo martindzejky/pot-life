aed2bbb10368eb4d90910fd39fbba8262cd6b5ee
Martin Jakubik <martindzejky@gmail.com>
2024-10-06 00:02:01 +0200

    add rocks and flowers

--- grass.gd
+++ grass.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+@export var minScale := 1
+@export var maxScale := 2
+
 func _ready():
 
     if randf() < 0.5:
         $sprite.flip_h = true
 
     $sprite.scale = Vector2(1, 1) * randf_range(1, 2)
-    $sprite.frame = randi_range(0, $sprite.hframes)
+    $sprite.frame = randi_range(0, $sprite.hframes - 1)
