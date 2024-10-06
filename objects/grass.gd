82e904ccee9ee3e5637bc7ebcf3e3b651795e956
Martin Jakubik <martindzejky@gmail.com>
2024-10-06 19:57:49 +0200

    add upgrade mechanic to grass, it grows now

--- objects/grass.gd
+++ objects/grass.gd
@@ -4,6 +4,8 @@ class_name Grass
 @export var minScale := 1.0
 @export var maxScale := 2.0
 
+var initialScale := 1.0
+
 # primitive state machine
 
 var state := ''
@@ -15,14 +17,25 @@ func _ready():
     if randf() < 0.5:
         $scale/sprite.flip_h = true
 
-    $scale.scale = Vector2(1, 1) * randf_range(minScale, maxScale)
-    $scale/sprite.frame = randi_range(0, $scale/sprite.hframes - 1)
     $energy.value = randf_range(10, 20)
+    initialScale = randf_range(minScale, maxScale)
+
+    $scale.scale = Vector2(1, 1) * initialScale
+    $scale/sprite.frame = randi_range(0, $scale/sprite.hframes - 1)
 
     switchState('idle')
 
 func _process(delta):
+
+    var previousScale = $scale.scale.x
+    var newScale := initialScale + floorf($energy.value / 30.0) / 4.0
+    if newScale > previousScale + 0.1:
+        $upgrade.play()
+
+    $scale.scale = Vector2(1, 1) * newScale
+
     call(state + 'Process', delta)
+
 
 func switchState(newState: String):
 
